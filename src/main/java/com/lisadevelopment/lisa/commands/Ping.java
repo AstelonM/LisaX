30decdfbefdc668dd631ce62b9f456a88aed3659
AstelonM <banicamihai96@gmail.com>
2019-06-25 15:35:44 +0300

    Added a command example

---
+++ src/main/java/com/lisadevelopment/lisa/commands/Ping.java
@@ -0,0 +1,22 @@
+package com.lisadevelopment.lisa.commands;
+
+import com.lisadevelopment.lisa.ChatListener;
+import com.lisadevelopment.lisa.ExecutionInstance;
+
+public class Ping extends Command {
+
+    public Ping(ChatListener listener) {
+        super(listener);
+        name = "ping";
+        description = "Get the ping of the bot.";
+        usage = listener.getPrefix() + "ping";
+        aliases = new String[] { "pong" };
+        examples = listener.getPrefix() + "ping";
+    }
+
+    @Override
+    public void treat(ExecutionInstance instance) {
+        listener.getJda().getRestPing().queue(ping ->
+                instance.getChannel().sendMessage("The ping is " + ping + " ms!").queue());
+    }
+}
