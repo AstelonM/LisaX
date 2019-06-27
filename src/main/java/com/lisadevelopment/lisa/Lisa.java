38c0ba6a5396c327be70b5936f1a4071b768ac05
ThatXuxe <obuoy1804@gmail.com>
2019-06-27 09:02:52 +0530

    Loading token from config.properties

--- src/main/java/com/lisadevelopment/lisa/Lisa.java
+++ src/main/java/com/lisadevelopment/lisa/Lisa.java
@@ -7,11 +7,21 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Lisa {
 
-    public static void main(String[] args) throws LoginException, InterruptedException {
-        JDA jda = new JDABuilder(Config.TOKEN).build().awaitReady();
+    public static void main(String[] args) throws LoginException, InterruptedException, IOException {
+        Properties properties = new Properties();
+        FileReader fileReader = new FileReader("config.properties");
+        properties.load(fileReader);
+        String token = properties.getProperty("token");
+        fileReader.close();
+        properties.clear();
+
+        JDA jda = new JDABuilder(token).build().awaitReady();
         ChatListener listener = new ChatListener(jda);
         listener.addCommand(new BotInfoCommands(listener));
         listener.addCommand(new GuildInfoCommands(listener));
