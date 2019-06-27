33514b3d771288f2f9ede59136ed2f41c48a2e86
slateyo16 <44808700+slateyo16@users.noreply.github.com>
2019-06-27 23:19:11 +0530

    almost completed

--- src/main/java/com/lisadevelopment/lisa/commands/guildinfo/GuildInfo.java
+++ src/main/java/com/lisadevelopment/lisa/commands/guildinfo/GuildInfo.java
@@ -22,6 +22,31 @@ public class GuildInfo extends Command {
         };
     }
 
+        private MessageEmbed getMemberInfo(Member member) {
+        User user = member.getUser();
+        String roleString = member.getRoles().stream().map(IMentionable::getAsMention)
+                .collect(Collectors.joining(", "));
+        String permissionString = member.getPermissions().stream().map(Permission::getName)
+                .collect(Collectors.joining(", "));
+        return new EmbedBuilder()
+                .setAuthor("Guild Info", null, guild.getIconUrl())
+                .setColor('random')
+                .setTimestamp(Instant.now())
+                .setThumbnail(guild.getIconUrl())
+                .setDescription(user.getAsMention() + " **(" + guild.getName() + "#" + user.getDiscriminator() + ")**")
+                .addField("Name", guild.getName(), true)
+                .addField("Server Region", guild.getRegion(), true)
+                .addField("Total members", guild.getMembers().stream().filter(member -> member.getUser()).count(), true)
+                .addField("Creation time",
+                        guild.getTimeCreated().format(DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss")), true)
+                .addField("Join time",
+                        member.getTimeJoined().format(DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss")), true)
+                .addField("Bot accounts", guild.getMembers().stream().filter(member -> member.getUser().isBot()).count(), true)
+                .addField("Roles", getRolesByName, false)
+                .addField("Permissions", permissionString, false)
+                .build();
+    }
+    
     @Override
     public void treat(ExecutionInstance instance) { //TODO to implement
         
